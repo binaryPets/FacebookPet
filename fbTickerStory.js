bb797b100574aeabfde8def8923e8a4b47dd9143
anouar1991 <anouar1991belhadj@gmail.com>
2017-07-24 02:08:59 +0200

    added fbTickerStoryFilter

---
+++ fbTickerStory.js
@@ -0,0 +1,19 @@
+var users = [ "Hiba Hassani", "Douaa Mimou", "Safaa Abir" ];
+
+setInterval(
+	function(){
+		fbThickerFilter(users);
+	}, 5000);
+
+
+function fbThickerFilter(users) {
+    for (i = 0; i < document.getElementsByClassName("fbFeedTickerStory").length; i++) {
+        userName = document.getElementsByClassName("fbFeedTickerStory")[i].getElementsByClassName('tickerFeedMessage')[0].getElementsByTagName('span')[0].innerText;
+        if (users.indexOf(userName)<0) {
+            document.getElementsByClassName("fbFeedTickerStory")[i].remove();
+            
+            console.log("[!] fb FeedTickerStory Removed\nUsername:"+userName);
+        }
+    }
+
+}
