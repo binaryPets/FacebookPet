37d482f97097bcff8cf83f1353b5d23ea2bc538d
anouar1991 <anouar1991belhadj@gmail.com>
2017-08-29 02:52:08 +0200

    fbtickerStory modified

--- fbTickerStory.js
+++ fbTickerStory.js
@@ -1,18 +1,18 @@
-var users = [ "Hiba Hassani", "Douaa Mimou", "Safaa Abir" ];
+var users = ["Hiba Hassani", "Douaa Mimou", "Safaa Abir"];
 
 setInterval(
-	function(){
-		fbThickerFilter(users);
-	}, 5000);
+    function() {
+        fbThickerFilter(users);
+    }, 5000);
 
 
 function fbThickerFilter(users) {
     for (i = 0; i < document.getElementsByClassName("fbFeedTickerStory").length; i++) {
         userName = document.getElementsByClassName("fbFeedTickerStory")[i].getElementsByClassName('tickerFeedMessage')[0].getElementsByTagName('span')[0].innerText;
-        if (users.indexOf(userName)<0) {
+        if (users.indexOf(userName) < 0) {
             document.getElementsByClassName("fbFeedTickerStory")[i].remove();
-            
-            console.log("[!] fb FeedTickerStory Removed\nUsername:"+userName);
+
+            console.log("[!] fb FeedTickerStory Removed\nUsername:" + userName);
         }
     }
 
